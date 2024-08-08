24e7c8a6da7118881b040e82c460053691e78297
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2024-08-08 20:27:23 +0530

    First Commit with Hero Section

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Header.css";
+import { assets } from "../../assets/assets";
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-left">
+        <span>Welcome to TasteTroop</span>
+        <h2>Delicious Food <br/> Delivered Fast & Fresh <br /> At Your Dorrstep</h2>
+        <p>Enjoy a hassle-free food ordering experience with quick delivery, diverse cuisine options, and secure payments.</p>
+        <button className='primary-btn'>View Menu</button>
+      </div>
+      <div className="header-right">
+        <img src={assets.hero_image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
