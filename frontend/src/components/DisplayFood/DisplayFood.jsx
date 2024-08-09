19964de69951917284e05b10f20ffea6a01b66ae
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2024-08-09 23:24:08 +0530

    Footer section

--- frontend/src/components/DisplayFood/DisplayFood.jsx
+++ frontend/src/components/DisplayFood/DisplayFood.jsx
@@ -3,23 +3,25 @@ import "./DisplayFood.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
-const DisplayFood = () => {
+const DisplayFood = ({ category }) => {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="display-food" id="display-food">
       <h2>Top Dishes near you</h2>
       <div className="display-food-list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              price={item.price}
-              desc={item.description}
-              image={item.image}
-            />
-          );
+          if (category === "all" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                price={item.price}
+                desc={item.description}
+                image={item.image}
+              />
+            );
+          }
         })}
       </div>
     </div>
