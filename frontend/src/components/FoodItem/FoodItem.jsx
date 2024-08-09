6692466e4e08e26f68d71ff9aa8d5702b803e0ec
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2024-08-09 15:57:11 +0530

    Food Items card

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useReducer, useState } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
+
+function countReducer(state, action) {
+  if (action.type === "increment") {
+    return { itemCount: state.itemCount + 1 };
+  } else if (action.type === "decrement") {
+    return { itemCount: state.itemCount - 1 };
+  }
+  throw Error("Unknown action.");
+}
+
+const FoodItem = ({ id, name, price, desc, image }) => {
+  //   const [itemCount, setItemCount] = useState(0);
+  const [state, dispatch] = useReducer(countReducer, { itemCount: 0 });
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="food-item">
+      <div className="food-item-image-container">
+        <img className="food-item-image" src={image} alt={name} />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-rating">
+          <h4>{name}</h4>
+          <img src={assets.rating_starts} alt="rating" />
+        </div>
+        <p className="food-item-desc">{desc}</p>
+        <div className="food-item-pricing">
+          <p className="food-item-price">&#8377;{price * 49}</p>
+          {!cartItems[id] ? (
+            <button
+              onClick={() => addToCart(id)}
+              className="primary-btn add-to-cart"
+            >
+              Add
+            </button>
+          ) : (
+            <div className="food-item-counter">
+              <button onClick={() => removeFromCart(id)}>
+                <span>&#8722;</span>
+              </button>
+              <p>{cartItems[id]}</p>
+              <button onClick={() => addToCart(id)}>
+                <span>&#43;</span>
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import MenuList from "../../components/MenuList/MenuList";
+import DisplayFood from "../../components/DisplayFood/DisplayFood";
 
 const Home = () => {
   const [category, setCategory] = useState("all");
@@ -10,6 +11,7 @@ const Home = () => {
     <div className="home">
       <Header />
       <MenuList category={category} setCategory={setCategory} />
+      <DisplayFood category={category} />
     </div>
   );
 };
