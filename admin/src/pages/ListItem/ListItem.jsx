78909117c7d6c42c49450fe740fa64289cd2c02e
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2024-08-20 19:38:20 +0530

    list food items

--- admin/src/pages/ListItem/ListItem.jsx
+++ admin/src/pages/ListItem/ListItem.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import "./ListItem.css";
+import axios from "axios";
 
 const ListItem = () => {
-  return <div>ListItem</div>;
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error(response.data.message);
+    }
+    console.log(response.data);
+  };
+
+  const removeFoodItem = async (foodId) => {
+    console.log(foodId);
+    const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="listitem add flex-col">
+      <p>All Food Items</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt={item.name} />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}</p>
+              <p className="close" onClick={() => removeFoodItem(item._id)}>
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
