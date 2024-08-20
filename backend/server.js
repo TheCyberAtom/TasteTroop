dcc0213c15d1b760be6c9afbb9469a49b0a57e39
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2024-08-20 13:57:45 +0530

    Adding backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+
+// App Config
+const app = express();
+const port = 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+connectDB();
+
+// API endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+// Run express server
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
+
+// mongodb+srv://tastetroop:gpwRsBVxKeTx9aUo@cluster0.7vhnn.mongodb.net/?
