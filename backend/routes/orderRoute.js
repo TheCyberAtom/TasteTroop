54601298c4e0804ea66ab5e5a70b3d73f79de86a
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2024-08-23 21:17:25 +0530

    list order backend logic

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder, userOrders } from "../controllers/orderController.js";
+import {
+  placeOrder,
+  userOrders,
+  listOrders,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
+orderRouter.post("/listOrders", listOrders);
 
 export default orderRouter;
